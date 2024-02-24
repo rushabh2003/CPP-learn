174648a935cd5c551aacc1a16f1411e9a2ac2363
Rushabh Aradwad <aradwadrushabh@gmail.com>
2024-02-24 13:46:42 +0530

    Lets go CPP

---
+++ hello.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+int main()
+{
+//comments
+/*
+Multiline comments
+*/
+int x =5 ;//declaration
+double y = 1.41;
+char a = 'A';
+bool Light = true;
+std::string A = " I am groot";
+std::cout<<"I am integer : \n"<<x<<"\nI am decimal(double) : \n"<<y<<"\nI am a character\n"<<a<<"\nI am boolean\n"<<Light<<"\nI am a string : \n"<<A;
+    return 0;
+}
