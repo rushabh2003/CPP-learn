940dc2f1f919ce7e857e84995e6ccf262d2594a7
Rushabh Aradwad <aradwadrushabh@gmail.com>
2024-02-26 12:12:47 +0530

    Euclid Method of GCD Calculation CP practical 3

--- hello.cpp
+++ hello.cpp
@@ -1,22 +1,17 @@
 #include <iostream>
 using namespace std;
-void collatzSequence(unsigned long n) {
-    while (n != 1) {
-        cout<<n<<endl;
-        if (n % 2 == 0) {
-            n = n / 2;
-        } else {
-            n = 3 * n + 1;
-        }
+int gcd(int a,int b){
+    if(b == 0){
+        return a;
+    }else{
+        return gcd(b,a%b);
     }
-    cout<<n<<"\n"<<endl; // Print the last number in the sequence, which is 1
-}
 
-int main() {
-    unsigned long n;
-    cout<<"Enter a positive integer: "<<endl;
-    cin>>n;;
-    cout<<"The 3n + 1 sequence starting at "<<n<<" is"<<endl;
-    collatzSequence(n);
-    return 0;
+}
+int main(){
+    int a,b;
+    cin>>a>>b;
+    int c = gcd(a,b);
+
+    cout<<c;
 }
