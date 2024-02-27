920a204576d5e5e55f5fe164b49e968d50197f4a
Rushabh Aradwad <aradwadrushabh@gmail.com>
2024-02-27 22:22:09 +0530

    Conditionals in Cpp(if,else,else if

--- hello.cpp
+++ hello.cpp
@@ -2,13 +2,15 @@
 #include <cmath>
 using namespace std;
 int main(){
-    double a,b;
-    cout<<"Enter Two Numbers"<<endl;
-    cin>>a>>b;
-    cout<<"The bigger number is  "<< max(a,b)<<endl;
-    cout<<"The smaller number is "<<min(a,b)<<endl;
-    cout<<"The a power b is "<<pow(a,b)<<endl;
-    cout<<"Square Root of A is "<<sqrt(a)<<endl;
-    //some useful functions are abs - absolute ,ceil() - upper rounding floor- downward rounding
+    double a;
+    cout<<"Enter your age "<<endl;
+    cin>>a;
+    if(a > 60){
+        cout<<"You must retire"<<endl;
+    }else if(a < 18){
+        cout<<"Go and Play"<<endl;
+    }else{
+        cout<<"Go workout ";
+    }
 
 }
