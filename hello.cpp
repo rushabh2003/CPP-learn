173352b759dd27186e1e36a64bb5038413318459
Rushabh Aradwad <aradwadrushabh@gmail.com>
2024-02-28 18:18:15 +0530

    Useful string functions

--- hello.cpp
+++ hello.cpp
@@ -1,16 +1,12 @@
 #include <iostream>
 #include <cmath>
 using namespace std;
-int main(){
-    //Logical operators and(&&) , or(||) and not(!)
-    cout<<"Enter a number "<<endl;
-    int a;
-    cin>>a;
-    if(a>18 && a<60){
-        cout<<"Go work"<<endl;
-    }else if(a<18 || a>60){
-        cout<<"Enjoy yourself "<<endl;
-    }else{
-        cout<<"Not operator is !"<<endl;
+int main()
+{
+    /* String methods (length(),empty(),clear(),append(),at(index),insert(index,string),find(),erase(index,index))*/
+    string a;
+    cout<<"Enter your name "<<endl;
+    getline(cin,a);
+    
+    cout<<a.erase(3,3);
     }
-}
