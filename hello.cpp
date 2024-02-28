2661a4033a6d5f58a9ae1979d9e329b995356862
Rushabh Aradwad <aradwadrushabh@gmail.com>
2024-02-28 18:51:24 +0530

    While loop

--- hello.cpp
+++ hello.cpp
@@ -1,12 +1,11 @@
 #include <iostream>
 #include <cmath>
 using namespace std;
-int main()
-{
-    /* String methods (length(),empty(),clear(),append(),at(index),insert(index,string),find(),erase(index,index))*/
+int main(){
     string a;
-    cout<<"Enter your name "<<endl;
-    getline(cin,a);
-    
-    cout<<a.erase(3,3);
+    while(a.empty()){
+        cout<<"Enter your name :"<<endl;
+        getline(cin,a);
     }
+    cout<<"Hello "<<a;
+}
