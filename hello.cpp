02a36ae0de0d9df6cb0a5297a07e77e89740d77d
Rushabh Aradwad <aradwadrushabh@gmail.com>
2024-02-28 19:37:00 +0530

    Functions in Cpp

--- hello.cpp
+++ hello.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
 #include <cmath>
 using namespace std;
+void Alpha(int n){
+    n -= 3;
+    cout<<n;
+}
 int main(){
-    for(int i = 0; i <= 10; i++){
-        if(i == 7)
-            continue;
-            //break
-        cout<<i<<endl;
-    }
+    Alpha(6);
 }
