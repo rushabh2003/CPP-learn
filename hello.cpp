449dffe13d8ed959f541d2b981e993f6ba280f00
Rushabh Aradwad <aradwadrushabh@gmail.com>
2024-03-01 14:10:26 +0530

    Sorting an array using Bubble sort algorithm

--- hello.cpp
+++ hello.cpp
@@ -1,9 +1,31 @@
 #include <iostream>
 #include <cmath>
 using namespace std;
-int main(){
-    //Arrays in Cpp
-    string  rga[] = {"rushabh","Om","Shambhu"};
-    //finding length of array
-    cout<<"length of given array is "<<sizeof(rga)/sizeof(string);    
+void bSort(int arr[],int size);
+int main()
+{
+    // bubble sort
+    int arr[] = {3, 2, 6, 7, 0, 1};
+    int size = sizeof(arr)/sizeof(int);
+
+    bSort(arr,size);
+    for (int a : arr)
+    {
+        cout << a << " ";
+    }
+}
+void bSort(int arr[], int size)
+{
+    int temp;
+    for (int i = 0; i < size - 1; i++)
+    {
+        for (int j = 0; j < size - i - 1; j++)
+        {
+            if (arr[j] > arr[j+1]){
+                temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
 }
