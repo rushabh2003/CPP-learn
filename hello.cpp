15b7a713fb9607577bfbefff05377cc457fc39fb
Rushabh Aradwad <aradwadrushabh@gmail.com>
2024-03-01 13:27:08 +0530

    Finding length of array using sizeOf function

--- hello.cpp
+++ hello.cpp
@@ -4,5 +4,6 @@ using namespace std;
 int main(){
     //Arrays in Cpp
     string  rga[] = {"rushabh","Om","Shambhu"};
-    cout<<rga[2];    
+    //finding length of array
+    cout<<"length of given array is "<<sizeof(rga)/sizeof(string);    
 }
