83bad80ab1a8b747a775e02fac3d750634036b53
Rushabh Aradwad <aradwadrushabh@gmail.com>
2024-03-05 18:16:10 +0530

    Memory addresses and pointers in cpp

--- hello.cpp
+++ hello.cpp
@@ -1,31 +1,13 @@
 #include <iostream>
-#include <cmath>
 using namespace std;
-void bSort(int arr[],int size);
+
 int main()
 {
-    // bubble sort
-    int arr[] = {3, 2, 6, 7, 0, 1};
-    int size = sizeof(arr)/sizeof(int);
-
-    bSort(arr,size);
-    for (int a : arr)
-    {
-        cout << a << " ";
-    }
-}
-void bSort(int arr[], int size)
-{
-    int temp;
-    for (int i = 0; i < size - 1; i++)
-    {
-        for (int j = 0; j < size - i - 1; j++)
-        {
-            if (arr[j] > arr[j+1]){
-                temp = arr[j];
-                arr[j] = arr[j+1];
-                arr[j+1] = temp;
-            }
-        }
-    }
+    //Memory addresses and pointers in cpp
+    int a = 1729;
+    int *ptr;//creating a pointer
+    ptr = &a;//storing address of variable into the pointer
+    cout<<"The given value is "<<a<<endl;
+    cout<<"The value is stored at "<<ptr<<endl;//hexadecimal memory address is returned
+    cout<<"The pointer points to the value  "<<*ptr<<endl;
 }
