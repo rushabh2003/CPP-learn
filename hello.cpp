8b508bfca284d6535d20818642fd2727af009656
Rushabh Aradwad <aradwadrushabh@gmail.com>
2024-03-06 19:24:32 +0530

    Dynamic memory for single object

--- hello.cpp
+++ hello.cpp
@@ -1,22 +1,15 @@
 #include <iostream>
 using namespace std;
-void passByValue(int x)
-{
-    x++;
-    cout<<"The value of (a) pass by value  is "<<x<<endl;
-}
-void passByRef(int &y)
-{
-    y++;
-    cout<<"The value of (b) pass by reference is "<<y<<endl;
-}
 
 int main()
 {
-    // Pass by Value and Pass by refference
-    int a = 3,b = 5;
-    passByValue(a);
-    cout<<"The value of a is "<<a<<endl;
-    passByRef(b);
-    cout<<"The value of b is "<<b<<endl;
+    //dynamic memory for single object
+    
+    int *ptr = new int; //allocating memory for int
+    *ptr = 5; //Assigning the value to  allocated memory
+    cout << "The value stored at dynamically allocated int is " << *ptr << endl;//Using the value
+    cout << "The address of value stored at dynamically allocated int is " << ptr;
+    delete ptr;//free the allocated memory 
+    ptr = nullptr; //Best practice to avoid undefined behaviour
+    return 0;
 }
