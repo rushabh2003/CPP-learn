412aebeccbbbe44c7e2e581b39b2f96c55868672
Rushabh Aradwad <aradwadrushabh@gmail.com>
2024-03-13 18:13:01 +0530

    Pair inputs

--- hello.cpp
+++ hello.cpp
@@ -5,6 +5,8 @@ using namespace std;
 //lets start STL - Standard Template Library
 int main()
 {
-    pair<int, int> t = {1, 2};
+    pair<int, int> t ;
+    cout<<"Enter values to pair : ";
+    cin>>t.first>>t.second;
     cout<<t.first<<endl<<t.second;
 }
